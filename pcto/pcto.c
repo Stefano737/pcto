5f6ce6b4e3c83332493593d0615a85fe7cb75034
Stefano737 <127745471+Stefano737@users.noreply.github.com>
2023-03-13 15:47:17 +0100

    Add files via upload

---
+++ pcto/pcto.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() 
+{
+    int numero = 3773;
+    double numerolungo = 597351793;
+    float numeroconlavirgola = 7.534;
+    char carattere ="c";
+
+    printf("il numero è: %d\n, numero");
+    printf("ïl numero lungo è: %f, numero con la virgola è: %f\n"", numerolungo, numero con la virgola);
+    printf("ïl carattere è %c\n, caratttere);)
+}
