dbeaea05177824d52cbcb7e1f06dc77e72148377
Stefano737 <127745471+Stefano737@users.noreply.github.com>
2023-03-14 12:29:44 +0100

    Add files via upload

---
+++ sium.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int anno;
+    printf("quale anno vuoi verificare?");
+    scanf("%d", &anno);
+    if(anno%400 == 0)
+    {
+        printf("l'anno è bisestile\n");
+    }
+    else if (anno%4 == 0 && anno%100 != 0)
+    {
+        printf("l'anno è bisestile\n");
+    }
+    else
+    {
+        printf("l'anno non è bisestile");
+    }
+    return 0;
+}
+
